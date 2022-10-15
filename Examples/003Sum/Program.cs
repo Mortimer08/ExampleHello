31d58a019bb73f7890b1700775d63825b82fea15
Ilya Makarov <mortimer08@gmail.com>
2022-10-15 11:12:14 +0500

    Random Added

--- Examples/003Sum/Program.cs
+++ Examples/003Sum/Program.cs
@@ -1,4 +1,8 @@
-﻿int numberA = 13;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,10);
+int numberB = new Random().Next(1,10);
 int result = numberA + numberB;
+Console.Write(numberA);
+Console.Write("+");
+Console.Write(numberB);
+Console.Write("=");
 Console.WriteLine(numberA + numberB);
