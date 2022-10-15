2a5dd9a4fe5725fd388298f837a247a7a64d77ae
Ilya Makarov <mortimer08@gmail.com>
2022-10-15 11:32:52 +0500

    Examole006 Added

---
+++ Examples/006ExampleBase/Program.cs
@@ -0,0 +1,16 @@
+﻿int A = 1;
+int B = 2;
+int C = 6;
+int D = 8;
+int E = 10;
+
+int Max = A;
+
+if(A > Max) Max = A;
+if(B > Max) Max = B;
+if(C > Max) Max = C;
+if(D > Max) Max = D;
+if(E > Max) Max = E;
+
+Console.Write("Max = ");
+Console.WriteLine(Max);   
