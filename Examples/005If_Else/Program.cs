46a02aa66c85fbfc1f8125897a18b31e995f7072
Ilya Makarov <mortimer08@gmail.com>
2022-10-15 11:26:09 +0500

    Example 005 Added

---
+++ Examples/005If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string UserName = Console.ReadLine();
+
+if(UserName.ToLower() == "маша")
+{   
+    Console.WriteLine("Давно не виделись, Маша!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(UserName);
+}
